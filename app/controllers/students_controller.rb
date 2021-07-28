c4b48843f67250ff1996ef1d20eddb2fa841e80a
Afzal Rahman <afzalrahman123@gmail.com>
2021-07-28 20:01:52 +0530

    Add flash messages and errors

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,23 +1,23 @@
 class StudentsController < ApplicationController
-    def index
-        @students = Student.all
+  def index
+    @students = Student.all
+  end
+  def new
+    @student = Student.new
+  end
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      flash[:notice] = "You've successfully signed up"
+      redirect_to root_path
+    else
+      render 'new'
     end
-    def new
-        @student = Student.new
-    end
-    def create
-      @student = Student.new(student_params)
-      if @student.save
-        flash[:success] = "You've successfully signed up"
-        redirect_to root_path
-      else
-        render 'new'
-      end
-    end
+  end
 
-    private
+  private
 
-    def student_params
-      params.require(:student).permit(:name, :email)
-    end
+  def student_params
+    params.require(:student).permit(:name, :email)
+  end
 end
