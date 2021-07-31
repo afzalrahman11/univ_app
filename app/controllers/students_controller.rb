863dc1a555d0a1accc3cd05c118fd9ab57388530
Afzal Rahman <afzalrahman123@gmail.com>
2021-07-31 12:17:51 +0530

    Add student show page

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,6 +2,9 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+  def show
+    @student = Student.find(params[:id])
+  end
   def new
     @student = Student.new
   end
