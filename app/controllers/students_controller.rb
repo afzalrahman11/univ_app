0a2d02a4f94838448b016aea6602f1058c65c310
Afzal Rahman <afzalrahman123@gmail.com>
2021-07-31 12:34:10 +0530

    Add edit and update to students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,13 +1,17 @@
 class StudentsController < ApplicationController
+
   def index
     @students = Student.all
   end
+
   def show
     @student = Student.find(params[:id])
   end
+
   def new
     @student = Student.new
   end
+
   def create
     @student = Student.new(student_params)
     if @student.save
@@ -15,6 +19,20 @@ class StudentsController < ApplicationController
       redirect_to root_path
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    if @student.update(student_params)
+      flash[:notice] = "You've successfully updated your profile"
+      redirect_to @student
+    else
+      render 'edit'
     end
   end
 
